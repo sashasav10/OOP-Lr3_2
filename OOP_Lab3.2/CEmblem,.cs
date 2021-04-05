a2c608c239062c9cffb1d5fd21c4acd22eaa1b7d
sashasav10 <58323503+sashasav10@users.noreply.github.com>
2021-04-06 00:07:57 +0300

    -first commit

---
+++ OOP_Lab3.2/CEmblem,.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Lab3._2
+{
+    class CEmblem
+    {
+        Color color { get; set; }
+        public Color currentColor { get; set; }
+        int size { get; set; }
+        int x { get; set; }
+        int y { get; set; }
+        string name { get; set; }
+        Pen pen = new Pen(Color.Blue, 1f);
+        public CEmblem(int x, int y, int size,string name, Color color)
+        {
+            this.x = x;
+            this.y = y;
+            this.size = size;
+            this.name = name;
+            this.color = color;
+            currentColor = color;
+        }
+        public override string ToString()
+        {
+            return name;
+        }
+        public void Draw(PictureBox pictureBox)
+        {
+            pen.Color = currentColor;
+            using (var g = Graphics.FromImage(pictureBox.Image))
+            {
+                g.DrawRectangle(pen, x, y, size, size);
+                g.DrawEllipse(pen, x, y, size, size);
+                Point[] points = new Point[3];
+                points[0].X = x+ size / 2; points[0].Y = y;
+                points[1].X = x+size / 15; points[1].Y = y+size / 4 * 3;
+                points[2].X = x+size -(size / 15); points[2].Y = y+size / 4 * 3;
+               g.DrawPolygon(pen, points);
+                pictureBox.Refresh();
+            }
+
+        }
+        public void MoveRight()
+        {
+            x++;
+        }
+        public void MoveLeft()
+        {
+            x--;
+        }
+        public void MoveUp()
+        {
+            y--;
+        }
+        public void MoveDown()
+        {
+            y++;
+        }
+        public void EndUp()
+        {
+            y = 0;
+        }
+        public void EndDown()
+        {
+            y = 455-size; 
+        }
+        public void EndLeft()
+        {
+            x = 0;
+        }
+        public void EndRight()
+        {
+            x = 623 - size;
+        }
+        public void Enlarge()
+        {
+            size++;
+        }
+        public void Reduce()
+        {
+            size--;
+        }
+        public void Hide()
+        {
+            currentColor = Color.White;
+        }
+        public void Show()
+        {
+            currentColor = color;
+        }
+    }
+}
---
+++ OOP_Lab3.2/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Lab3._2
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            CEmblem figure = (CEmblem)comboBox1.SelectedItem;
+            if (figure.currentColor == Color.White) { return; }
+            figure.Hide();
+            figure.Draw(pictureBox1);
+            figure.EndUp();
+            figure.Show();
+            figure.Draw(pictureBox1);
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            CEmblem figure = (CEmblem)comboBox1.SelectedItem;
+            if (figure.currentColor == Color.White) { return; }
+            figure.Hide();
+            figure.Draw(pictureBox1);
+            figure.Enlarge();
+            figure.Show();
+            figure.Draw(pictureBox1);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            int randSize = r.Next(40, 200);
+            int rand = r.Next(0, 623-randSize);
+            int rand2 = r.Next(0, 455-randSize);
+            
+            CEmblem figure = new CEmblem(rand, rand2, randSize, "Фігура " + count, Color.DarkBlue);
+            figure.Show();
+            figure.Draw(pictureBox1);
+            comboBox1.Items.Add(figure);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            CEmblem figure = (CEmblem)comboBox1.SelectedItem;
+            figure.Hide();
+            figure.Draw(pictureBox1);
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            CEmblem figure = (CEmblem)comboBox1.SelectedItem;
+            figure.Show();
+            figure.Draw(pictureBox1);
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            CEmblem figure = (CEmblem)comboBox1.SelectedItem;
+            if (figure.currentColor == Color.White) { return; }
+            figure.Hide();
+            figure.Draw(pictureBox1);
+            figure.Reduce();
+            figure.Show();
+            figure.Draw(pictureBox1);
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            CEmblem figure = (CEmblem)comboBox1.SelectedItem;
+            if (figure.currentColor == Color.White) { return; }
+            figure.Hide();
+            figure.Draw(pictureBox1);
+            figure.MoveUp();
+            figure.Show();
+            figure.Draw(pictureBox1);
+        }
+
+        private void Button11_Click(object sender, EventArgs e)
+        {
+            CEmblem figure = (CEmblem)comboBox1.SelectedItem;
+            if (figure.currentColor == Color.White) { return; }
+            figure.Hide();
+            figure.Draw(pictureBox1);
+            figure.MoveLeft();
+            figure.Show();
+            figure.Draw(pictureBox1);
+        }
+
+        private void Button12_Click(object sender, EventArgs e)
+        {
+            CEmblem figure = (CEmblem)comboBox1.SelectedItem;
+            if (figure.currentColor == Color.White) { return; }
+            figure.Hide();
+            figure.Draw(pictureBox1);
+            figure.MoveRight();
+            figure.Show();
+            figure.Draw(pictureBox1);
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            CEmblem figure = (CEmblem)comboBox1.SelectedItem;
+            if (figure.currentColor == Color.White) { return; }
+            figure.Hide();
+            figure.Draw(pictureBox1);
+            figure.MoveDown();
+            figure.Show();
+            figure.Draw(pictureBox1);
+        }
+
+        private void Button10_Click(object sender, EventArgs e)
+        {
+            CEmblem figure = (CEmblem)comboBox1.SelectedItem;
+            if (figure.currentColor == Color.White) { return; }
+            figure.Hide();
+            figure.Draw(pictureBox1);
+            figure.EndLeft();
+            figure.Show();
+            figure.Draw(pictureBox1);
+
+        }
+
+        private void Button13_Click(object sender, EventArgs e)
+        {
+            CEmblem figure = (CEmblem)comboBox1.SelectedItem;
+            if (figure.currentColor == Color.White) { return; }
+            figure.Hide();
+            figure.Draw(pictureBox1);
+            figure.EndRight();
+            figure.Show();
+            figure.Draw(pictureBox1);
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            CEmblem figure = (CEmblem)comboBox1.SelectedItem;
+            if (figure.currentColor == Color.White) { return; }
+            figure.Hide();
+            figure.Draw(pictureBox1);
+            figure.EndDown();
+            figure.Show();
+            figure.Draw(pictureBox1);
+        }
+    }
+}
